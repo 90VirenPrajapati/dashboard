bc8b81db87ffb3dc7624171b25f70b3277eb3c3c
viren <viren.viru.19@gmail.com>
2024-01-10 18:46:20 +0530

    -m

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -3,26 +3,26 @@ import Box from "@mui/material/Box";
 import Sidebar from "../components/Sidebar";
 import Grid from "@mui/material/Grid";
 import {Card, CardContent, Stack} from "@mui/material";
-import BarChart from "../components/BarChart";
-import Accordino from "../components/Accordino";
 import LineChart from "../components/LineChart";
 import PieChart from "../components/PieChart";
 import ScatterCharts from "../components/ScatterChart";
+import Line from "../components/Line";
 
 const About = () => {
     return (
         <>
-            <Box sx={{display:'flex'}}>
+            <Box sx={{display: 'flex'}}>
                 <Sidebar/>
-                <Box component="main" sx={{ flexGrow: 1}}>
+                <Box component="main" sx={{flexGrow: 1}}>
                     <Box>
                         <Box sx={{display: 'flex'}}>
                             <Box component="main" sx={{flexGrow: 1, p: 10, background: '#eeeee4'}}>
                                 <Grid container spacing={2}>
                                     <Grid item xs={8}>
                                         <Stack spacing={2}>
-                                                  <PieChart/>
-                                            <Card className='paddingall' elevation={3} sx={{minWidth: 49 + '%', height: 140}}>
+                                            <PieChart/>
+                                            <Card className='paddingall' elevation={3}
+                                                  sx={{minWidth: 49 + '%', height: 140}}>
                                                 <CardContent>
                                                     <div>
                                                         <span className='pricetitle'> $203K </span><br/>
@@ -34,14 +34,8 @@ const About = () => {
                                     </Grid>
                                     <Grid item xs={4}>
                                         <Stack spacing={2}>
-                                            <Card elevation={3} sx={{maxWidth: 500}}>
-                                                    <ScatterCharts/>
-                                            </Card>
-                                            <Card elevation={3} sx={{maxWidth: 500}}>
-                                                <div className='paddingall'>
-                                                    <span className='pricetitle'> $203K </span><br/>
-                                                    <span className='pricesubtitle'> Total Income </span>
-                                                </div>
+                                            <Card sx={{maxWidth: 500}}>
+                                                <ScatterCharts/>
                                             </Card>
                                         </Stack>
                                     </Grid>
@@ -54,15 +48,12 @@ const About = () => {
                                     </Grid>
                                     <Grid item xs={4}>
                                         <Card elevation={3} sx={{height: 60 + 'vh'}}>
-                                            <CardContent>
-                                                <Accordino/>
-                                            </CardContent>
+                                            <Line/>
                                         </Card>
                                     </Grid>
                                 </Grid>
                                 <br/>
                             </Box>
-
                         </Box>
                     </Box>
                 </Box>
